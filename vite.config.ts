736cb7be1248d1ec0cee0f8b9823247f3eb5e917
Wang Guan <momocraft@gmail.com>
2024-02-19 22:38:23 +0900

    format config

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,31 @@
-import { defineConfig, splitVendorChunkPlugin } from "vite";
-import react from '@vitejs/plugin-react'
-import path from "node:path";
-import { antdLessVars, antdLessVarsM} from './src/style';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { antdLessVars, antdLessVarsM } from './src/style';
 import vitePluginImp from 'vite-plugin-imp';
 import { visualizer } from 'rollup-plugin-visualizer';
 import url from 'node:url';
-const ___dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
+const ___dirname = path.dirname(url.fileURLToPath(import.meta.url));
 const componentsDir = path.join(__dirname, './src/components');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // root: 'src',
   build: {
-    outDir: path.join(__dirname, "./build"),
+    outDir: path.join(__dirname, './build'),
     emptyOutDir: true,
     rollupOptions: {
       // external: ['lodash', 'lodash/default'],
       output: {}
-    },
+    }
+    // minify: 'terser'
+  },
+  define: {
+    'process.env.REACT_APP_BASE_URL': JSON.stringify(process.env.REACT_APP_BASE_URL ?? '/api/')
   },
   resolve: {
-    alias: {
-    },
+    alias: {}
   },
   plugins: [
     vitePluginImp({
@@ -30,20 +33,20 @@ export default defineConfig({
         {
           // antd 按需导入
           libName: 'antd',
-          style: (name) => `antd/es/${name}/style`,
+          style: (name) => `antd/es/${name}/style`
         },
         {
           // antd-mobile 按需导入
           libName: 'antd-mobile',
-          style: (name) => `antd-mobile/es/${name}/style`,
-        },
-      ],
+          style: (name) => `antd-mobile/es/${name}/style`
+        }
+      ]
     }),
     react({
-      jsxImportSource: '@emotion/core',
+      jsxImportSource: '@emotion/core'
     }),
     visualizer({}),
-    splitVendorChunkPlugin(),
+    splitVendorChunkPlugin()
   ],
   css: {
     preprocessorOptions: {
@@ -51,17 +54,17 @@ export default defineConfig({
         // 覆盖 antd 的 Less 样式
         javascriptEnabled: true,
         modifyVars: ({
-          ...antdLessVars,...antdLessVarsM
-        }),
-      },
-    },
+          ...antdLessVars, ...antdLessVarsM
+        })
+      }
+    }
   },
   server: {
     proxy: {
-      '/api': {
-        // a server running moeflow-backend
-        target: 'https://moeflow.ihate.work',
-        changeOrigin: true,
+      '/api/': {
+        // in local dev, proxy local moeflow-backend server for web app
+        target: 'http://localhost:5000',
+        changeOrigin: true
       }
     }
   }
