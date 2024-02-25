d43f3a92ed0145c920ce11815585aa99e7587ed5
Wang Guan <momocraft@gmail.com>
2024-02-25 20:12:22 +0900

    add not found page

---
+++ src/pages/404.tsx
@@ -0,0 +1,20 @@
+import { useTitle } from '../hooks';
+import { useHistory } from 'react-router-dom';
+import { FC, useEffect } from 'react';
+
+export const NotFoundPage: FC<{}> = (props) => {
+  const history = useHistory();
+  useTitle({
+    prefix: 'Page not found',
+  });
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      history.push('/');
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+  return <div>Page not found. You will be redirected shortly</div>;
+};
