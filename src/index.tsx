2d86af85241ade83251135e493bcf804b713263a
Wang Guan <momocraft@gmail.com>
2024-05-18 19:43:59 +0900

    make prettier happy

--- src/index.tsx
+++ src/index.tsx
@@ -46,9 +46,11 @@ dayjs.extend(relativeTime);
 dayjs.extend(utc);
 dayjs.locale(locale.toLowerCase());
 if (false && process.env.NODE_ENV === 'development') {
-    // 用于检测是什么导致 re-render
-    const {default: whyDidYouRender} = await import('@welldone-software/why-did-you-render');
-    whyDidYouRender(React, { trackAllPureComponents: true });
+  // 用于检测是什么导致 re-render
+  const { default: whyDidYouRender } = await import(
+    '@welldone-software/why-did-you-render'
+  );
+  whyDidYouRender(React, { trackAllPureComponents: true });
 }
 // 将 Cookie 中 token 恢复到 Store
 const cookieToken = getToken();
