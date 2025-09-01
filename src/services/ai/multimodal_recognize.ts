26446ee877e3e57906db8a75d47d9d6f7ffb9f0a
Wang Guan <momocraft@gmail.com>
2025-09-02 02:18:52 +0900

    remove old code & add multimodal_recognize service

--- src/services/ai/multimodal_recognize.ts
+++ src/services/ai/multimodal_recognize.ts
@@ -1,10 +1,13 @@
+import z from "zod";
+import { generateObject, GenerateObjectOptions, UserMessage} from 'xsai'
+
 interface MultimodalModelConf {
   provider: string;
   model: string;
   baseUrl: string;
 }
 
-export const multimodalPresets: readonly MultimodalModelConf[] = [
+export const multimodalPresets: readonly Readonly<MultimodalModelConf>[] = [
   // gemini:
   // see https://ai.google.dev/gemini-api/docs/openai
   {
@@ -19,4 +22,36 @@ export const multimodalPresets: readonly MultimodalModelConf[] = [
   },
 ];
 
-export async function x();
+const fileRecognizeResultSchema = z.object({
+    imageW: z.number({message: 'the width of the image in PX'}),
+    imageH: z.number({message: 'the height of the image in PX'}),
+    texts: z.array(z.object({
+      left: z.number({message: 'left coordinate of the text in PX, in the whole image'}),
+      top: z.number({message: 'top coordinate of the text in PX, in the whole image'}),
+      width: z.number({message: 'width of the text in PX'}),
+      height: z.number({message: 'height of the text in PX'}),
+      textLines: z.array(z.string(), {message: 'the text lines'}),
+      text: z.string({message: 'concatencated text'}),
+      tranalated: z.string({message: 'translated text'}),
+      comment: z.string({message: 'additional comment of the text, or the translation'}),
+    }))
+})
+
+export async function recognizeFile(apiKey: string, conf: MultimodalModelConf, msg: UserMessage, abortSignal?: AbortSignal): Promise<z.infer<typeof fileRecognizeResultSchema>> {
+  const generateConf: GenerateObjectOptions<typeof fileRecognizeResultSchema> = {
+    messages: [
+      { content: 'You are a helpful assistant. Please do as user instructs.', role: 'system' },
+      msg,
+    ], 
+    schema: fileRecognizeResultSchema,
+    baseURL: conf.baseUrl,
+    model: conf.model,
+    apiKey,
+  }
+  const res = await generateObject({
+    ...generateConf,
+    abortSignal
+  })
+  return res.object
+}
+
