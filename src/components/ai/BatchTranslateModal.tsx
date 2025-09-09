6061dd702d3920c16393a4a34dc9a0ceddfa513c
Wang Guan <momocraft@gmail.com>
2025-09-10 02:40:23 +0900

    revise translate modal

--- src/components/ai/BatchTranslateModal.tsx
+++ src/components/ai/BatchTranslateModal.tsx
@@ -10,37 +10,44 @@ import { createDebugLogger } from '@/utils/debug-logger';
 import { api, resultTypes } from '@/apis';
 import { toLowerCamelCase } from '@/utils';
 import {
-  testModel,
   llmPreprocessFile,
   LLMConf,
   FilePreprocessResult,
 } from '@/services/ai/llm_preprocess';
 import { ModalHandle } from '.';
 import { UserMessage } from 'xsai';
+import { Icon } from '../icon';
 
 const debugLogger = createDebugLogger('components:ai:BatchTranslateModal');
-interface TranslateTaskState {
+interface FileProgress {
   file: MFile;
-  status: string;
+  icon: React.ReactNode | string
+  message?: React.ReactNode | string;
 }
 
 function clipTo01(x: number) {
   return Math.max(0, Math.min(1, x));
 }
 
+const stateIcons = {
+  waiting: <Icon icon="ellipsis-h" bounce />,
+  working: <Icon icon="spinner" spin />,
+  skip: <Icon icon="exclamation-circle" />,
+  error: <Icon icon="exclamation-square" />,
+  success: <Icon icon="check" />,
+} as const
+
 export const BatchTranslateModalContent: FC<{
   llmConf: LLMConf;
-
   files: MFile[];
   target: Target;
   getHandle(): ModalHandle;
 }> = ({ files, target, getHandle, llmConf }) => {
   const intl = useIntl();
-  const [fileStates, setFileStates] = useState<TranslateTaskState[]>(() =>
-    files.map((file) => ({ file, status: 'waiting' })),
+  const [fileStates, setFileStates] = useState<FileProgress[]>(() =>
+    files.map((file): FileProgress => ({ file, icon: stateIcons.waiting, message: 'waiting' })),
   );
 
-  async function startWork() {}
   useAsyncEffect(async (running, released) => {
     const [cancelToken, fillCancelToken] = getCancelToken();
     const fileLimiter = ResourcePool.multiple([1, 2]);
@@ -53,48 +60,54 @@ export const BatchTranslateModalContent: FC<{
       debugLogger('canceled');
       return;
     }
-    const tasksEnded = Promise.allSettled([
+    released = released.then(() => {
+      debugLogger('released')
+    })
+    const tasksEnded = Promise.allSettled(
       files.map((f, idx) => fileLimiter.use(() => translateFile(f, idx))),
-    ]);
+    );
     const cancelled = await Promise.race([
       released.then(() => true),
       tasksEnded.then(() => false),
     ]);
+    debugLogger('cancelled', cancelled);
     if (!cancelled) {
       const handle = getHandle();
       handle.update({ okButtonProps: { disabled: false } });
     }
     return;
 
-    function setFileState(f: MFile, status: string) {
+    function setFileState(f: MFile, message: string, icon: React.ReactNode) {
+      debugLogger('setFileState', f.id, message);
       setFileStates((prev) =>
-        prev.map((state) => (state.file === f ? { ...state, status } : state)),
+        prev.map((state) => (state.file === f ? { ...state, message, icon } : state)),
       );
     }
 
     async function translateFile(f: MFile, idx: number) {
-      setFileState(f, 'working');
+      setFileState(f, 'working', stateIcons.working);
       if (![undefined, null, 'success'].includes(f.uploadState)) {
-        setFileState(f, 'skip: upload not finished');
+        setFileState(f, 'skip: upload not finished', stateIcons.skip);
         return;
       }
       const refetchRes = await api.file
-        .getFile({ fileID: f.id })
+        .getFile({ fileID: f.id, configs: { cancelToken } })
         .catch(() => null);
       if (refetchRes?.type !== resultTypes.SUCCESS) {
-        setFileState(f, 'skip: fetch file failed');
+        setFileState(f, 'skip: fetch file failed', stateIcons.error);
         return;
       }
       const resData = toLowerCamelCase(refetchRes.data);
       if (resData.sourceCount) {
-        setFileState(f, 'skip: source count not 0');
+        setFileState(f, 'skip: already has source', stateIcons.skip);
+        return
       }
-      const imgBlob = await fetch(resData.url!, {}).then(
+      const imgBlob = await fetch(resData.url!, {signal: abort.signal }).then(
         (r) => r.blob(),
         () => null,
       );
       if (!imgBlob) {
-        setFileState(f, 'skip: fetch image blob failed');
+        setFileState(f, 'error: fetch image blob failed', stateIcons.error);
         return;
       }
 
@@ -114,6 +127,8 @@ export const BatchTranslateModalContent: FC<{
         ],
       };
 
+      setFileState(f, 'translating', stateIcons.working);
+
       const result = await llmPreprocessFile(llmConf, userMessage).catch(
         (e) => {
           debugLogger('translate failed', e);
@@ -121,11 +136,14 @@ export const BatchTranslateModalContent: FC<{
         },
       );
       debugLogger('translate result', result);
+      if (!running.current) {
+        return
+      }
 
       if (result) {
         await saveTranslations(f, result);
       } else {
-        setFileState(f, 'error: translate failed');
+        setFileState(f, 'error: translate failed', stateIcons.error);
       }
     }
 
@@ -149,35 +167,38 @@ export const BatchTranslateModalContent: FC<{
           content: tb.translated,
           targetID: target.id,
         },
-        configs: { cancelToken },
+        // not using the cancel token, to make the saving operation closer to atomic
+        // configs: { cancelToken },
       });
     }
 
     async function saveTranslations(f: MFile, r: FilePreprocessResult) {
       if (r.texts.length === 0) {
-        setFileState(f, 'done: no text blocks');
+        setFileState(f, 'done: no text blocks', stateIcons.skip);
       }
-      setFileState(f, 'saving');
+      setFileState(f, 'saving', stateIcons.working);
       try {
         await Promise.all(
           r.texts.map((tb) =>
             moeflowApiLimiter.use(() => saveTextBlock(f, r, tb)),
           ),
         );
-        setFileState(f, `success: translated ${r.texts.length} text marks`);
+        setFileState(f, `success: recognized ${r.texts.length} text marks`, stateIcons.success);
       } catch (e) {
         debugLogger('save text block failed', e);
-        setFileState(f, 'save file failed');
+        setFileState(f, 'save file failed', stateIcons.error);
       }
     }
   }, []);
   return (
     <div>
-      {files.length} files to translate
+      <p>Translating {files.length} files with LLM. Closing this dialog will stop translating.</p>
       <ul>
         {fileStates.map((state) => (
-          <li key={state.file.id}>
-            {state.file.name} - {state.status}
+          <li key={state.file.id} >
+            <span style={{margin: '0 4px'}} >
+            {state.icon}</span>
+            {state.file.name} - {state.message}
           </li>
         ))}
       </ul>
